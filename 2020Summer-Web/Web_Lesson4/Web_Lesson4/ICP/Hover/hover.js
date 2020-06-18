bdc1f0307095300c93f6bf7360ef3f93282bd851
RLS <rod.skoglund@gmail.com>
2020-06-18 10:29:07 -0500

    Hover Image task complete

--- 2020Summer-Web/Web_Lesson4/Web_Lesson4/ICP/Hover/hover.js
+++ 2020Summer-Web/Web_Lesson4/Web_Lesson4/ICP/Hover/hover.js
@@ -1,3 +1,10 @@
+/******************************************************************************/
+/* Class - Web and Mobile Programming                                         */
+/* Semester / School - Summer 2020 / UMKC                                     */
+/* Assignment: Web-Lesson3                                                    */
+/* Author: Rod Skoglund                                                       */
+/* File: hover.js for the Hover Image task                                    */
+/******************************************************************************/
 function upDate(previewPic) {
     /* In this function you should
        1) change the url for the background image of the div with the id = "image"
@@ -7,6 +14,10 @@ function upDate(previewPic) {
        to the alt text of the preview image
        */
 
+    /* Set the background image and the text to match the item you are        */
+    /* hovering over (passed in as an argument)                               */
+    document.getElementById("image").innerHTML = `${previewPic.alt}`;
+    document.getElementById("image").style.backgroundImage = `url(${previewPic.src})`;
 }
 
 function unDo() {
@@ -17,5 +28,9 @@ function unDo() {
    2) Change the text  of the div with the id = "image"
    back to the original text.  You can use the html code to see what that original text was
    */
+
+    /* Reset the background image and the text back to the initial settings   */
+    document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+    document.getElementById("image").style.backgroundImage = `url('')`;
 
 }
